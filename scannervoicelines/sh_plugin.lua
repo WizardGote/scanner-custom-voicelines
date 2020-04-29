a512a65caa410b8adc579316058b2c9f211f60cd
Miles Valentine <InvaGirSP@gmail.com>
2020-04-30 02:46:48 +0300

    Add files via upload

---
+++ scannervoicelines/sh_plugin.lua
@@ -0,0 +1,21 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Custom Scanner Voicelines"
+PLUGIN.author = "Ceryx"
+PLUGIN.description = "Adds custom voicelines to the metropolice scanner class and also remove their ability to use the MPF voicelines"
+
+ix.util.Include("sh_voices.lua")
+
+--[[
+
+Please do not reupload this plugin or the sounds to any other public platform.
+If you wish to share it with others, share with this link: https://github.com/Ceryxat/scanner-custom-voicelines
+
+The code may not be mine originally but the sounds are, I've made them hence they belong to me.
+Failure to comply with the above conditions will result with violating my copyrights
+I grant premission to use this plugin for free solely for the purpose it was made for and only that.
+
+In order for the sounds to work, you need to download this addon to your server and to your own computer: https://steamcommunity.com/sharedfiles/filedetails/?id=2078687121
+After installation, if the sounds don't work try to restart the game and try again.
+
+]] --
